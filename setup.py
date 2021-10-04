8507beaf24482bbcaf9fc1d0e463b58598de5857
st4rb4rs <91555200+st4rb4rs@users.noreply.github.com>
2021-10-04 10:30:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='okokokokok',
+    version='0.1.1',
+    description='Simple choose your own story game',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Jack Adkins',
+    author_email='barsofthestars.xyz@gmail.com',
+    url='https://github.com/st4rb4rs/okokokokok',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
